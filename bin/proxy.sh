49dae0b9422745eaadd88ebcf415f6cbb30b4f64
Vladimir Kotal <vlada@kotalovi.cz>
2025-03-24 11:25:15 +0100

    replace netcat with socat

--- bin/proxy.sh
+++ bin/proxy.sh
@@ -7,10 +7,7 @@
 #  Hostname github.openssl.org
 #  ProxyCommand ~/bin/proxy.sh %h %p
 #
-# Needs socat and OpenBSD or GNU Netcat.
-#
-# Note: does not work with the Netcat shipped with macOS.
-#       Have to use GNU Netcat from Homebrew.
+# Requires socat.
 #
 
 if (( $# != 2 )); then
@@ -18,36 +15,20 @@ if (( $# != 2 )); then
 	exit 1
 fi
 
-# TODO: remove
-function check_vpn()
-{
-	ip link show vpn0 >/dev/null 2>&1
-	if (( $? != 0 )); then
-		vpn=0
-	else
-		ip link show vpn0 | grep NO-CARRIER >/dev/null
-		if (( $? != 0 )); then
-			vpn=1
-		fi
-	fi
-
-	return vpn
-}
-
-# vpn = check_vpn()
-
-use_proxy=0
-# nc -w 1 -z -n $PROXY_IP $PROXY_PORT
-# socat is faster than netcat because it allows sub-second connect timeout.
-socat /dev/null TCP-CONNECT:$VPN_PROXY_IP:$VPN_PROXY_PORT,connect-timeout=0.1 2>/dev/null
-if (( $? == 0 )); then
-	use_proxy=1
+socat=$( which socat )
+if [[ -z $socat ]]; then
+	echo "Install socat first"
+	exit 1
 fi
 
-# TODO: replace netcat with socat
-if (( use_proxy != 0 )); then
-	# print "using proxy"
-	exec nc -X connect -x $VPN_PROXY_IP:$VPN_PROXY_PORT $1 $2
+use_proxy=0
+#
+# socat is faster in detecting whether proxy is responding than netcat
+# because it allows sub-second connect timeout.
+#
+socat /dev/null TCP-CONNECT:$VPN_PROXY_IP:$VPN_PROXY_PORT,connect-timeout=0.1 2>/dev/null
+if (( $? == 0 )); then
+	exec socat - PROXY:$VPN_PROXY_IP:$1:$2,proxyport=$VPN_PROXY_PORT
 else
 	exec nc $1 $2
 fi
